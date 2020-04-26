2c55c8e90e5bc2fd2ab7728d9662ff5d57d33d09
Alex <19fi.o.rutkas@std.npu.edu.ua>
2020-04-26 18:23:32 +0300

    LAB3 created

---
+++ src/edu/LABS/LAB3/Cat.java
@@ -0,0 +1,14 @@
+package edu.labs.LAB3;
+
+public class Cat implements Walking, Talking {
+
+    @Override
+    public void walk() {
+        System.out.println("Cat is walking... method works.");
+    }
+
+    @Override
+    public void talk() {
+        System.out.println("I am cat. Play with me!");
+    }
+}
---
+++ src/edu/LABS/LAB3/Turtle.java
@@ -0,0 +1,9 @@
+package edu.labs.LAB3;
+
+public class Turtle implements Swimming {
+
+    @Override
+    public void swim() {
+        System.out.println("Turtle is swimming... method works.");
+    }
+}
