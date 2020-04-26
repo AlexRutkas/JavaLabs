30dec81b8d785387160d523b4359d9d1f5fa54bc
Alex <19fi.o.rutkas@std.npu.edu.ua>
2020-04-26 18:34:22 +0300

    LAB4 added

---
+++ src/edu/LABS/LAB4/InStack.java
@@ -0,0 +1,8 @@
+package edu.labs.LAB4;/* package labs;
+
+interface InStack<E> {
+    boolean empty();
+    E peek();
+    E pop();
+    E push(E value);
+}*/
---
+++ src/edu/LABS/LAB4/Stack.java
@@ -0,0 +1,9 @@
+package edu.labs.LAB4;
+
+public interface Stack {
+    public void push(int data);
+    public int pop();
+    public int size();
+    public boolean isEmpty();
+    public void clear();
+}
